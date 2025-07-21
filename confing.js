c7095ea3d14a1680c6d092d1fb34fb2a9ba46c2c
lakshankpg <lakshankpg@gmail.com>
2025-07-21 20:32:25 +0530

    Update confing.js

--- confing.js
+++ confing.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
+  SESSION_ID: process.env.SESSION_ID || "rd0AmbxQ#Q9qxdMFGeMCm4W_QTpT4lFRVpk0j8CDdbm8wzLBTUoo",
+  MONGODB: process.env.MONGODB || "mongodb+srv://ashenikavindi00:b1ajNVz40nKVlwjf@cluster0.rvzcqfk.mongodb.net/",
+  OWNER_NUM: process.env.OWNER_NUM || "94763441376",
 };
